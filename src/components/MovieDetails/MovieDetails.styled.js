d00499b05e08a19f82739843569d897a71514c3d
Aleksandr Shereshkov <aleksandr.shereshkov@gmail.com>
2022-08-14 19:20:02 +0300

    Revert "fix"
    This reverts commit 4a515fabb01d74b961497b4862806b38abd96b2d.

--- src/components/MovieDetails/MovieDetails.styled.js
+++ src/components/MovieDetails/MovieDetails.styled.js
@@ -40,67 +40,108 @@ export const Label = styled.span`
 
 export const MainWrapper = styled.div`
   margin-bottom: 100px;
-  display: flex;
+
+  @media (min-width: 768px) {
+    display: flex;
+  }
 `;
 
 export const Poster = styled.img`
-  width: 400px;
+  width: 300px;
+
+  @media (min-width: 768px) {
+    width: 400px;
+  }
 `;
 
 export const InfoWrapper = styled.div`
-  margin-left: 50px;
+  @media (min-width: 768px) {
+    margin-left: 50px;
+  }
 `;
 
 export const TitleWrapper = styled.div`
   margin-bottom: 30px;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
+
+  @media (min-width: 768px) {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
 `;
 
 export const Title = styled.h2`
+  font-size: 30px;
   color: ${accentTextColor};
+  margin-bottom: 10px;
 
-  font-size: 50px;
-  margin-bottom: 0;
+  @media (min-width: 768px) {
+    font-size: 25px;
+    margin-bottom: 0;
+  }
+
+  @media (min-width: 1440px) {
+    font-size: 50px;
+    margin-bottom: 0;
+  }
 `;
 
 export const ReleaseDate = styled.p`
   font-weight: 700;
-  font-size: 18px;
+  font-size: 15px;
+
+  @media (min-width: 1440px) {
+    font-size: 18px;
+  }
 `;
 
 export const GenresList = styled.ul`
   margin-bottom: 50px;
-  display: flex;
+
+  @media (min-width: 768px) {
+    display: flex;
+  }
 `;
 
 export const GenresItem = styled.li`
   font-weight: 700;
-
+  font-size: 15px;
   margin-right: 15px;
 
   :last-child {
     margin-right: 0;
   }
 
-  font-size: 18px;
+  @media (min-width: 1440px) {
+    font-size: 18px;
+  }
 `;
 
 export const Description = styled.p`
   line-height: 2;
+  font-size: 15px;
   margin-bottom: 50px;
-  font-size: 18px;
+
+  @media (min-width: 1440px) {
+    font-size: 18px;
+  }
 `;
 
 export const VoteWrapper = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
+  @media (min-width: 768px) {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
 `;
 
 export const VoteAverage = styled.p`
   font-size: 18px;
+  margin-bottom: 10px;
+
+  @media (min-width: 768px) {
+    margin-bottom: 0;
+  }
 `;
 
 export const VoteCount = styled.p`
--- src/components/MoviesList/MoviesList.styled.js
+++ src/components/MoviesList/MoviesList.styled.js
@@ -1,7 +1,7 @@
 import styled from '@emotion/styled';
 
 export const Wrapper = styled.ul`
-  min-width: 768px {
+  @media (min-width: 768px) {
     display: flex;
     flex-wrap: wrap;
     margin: -10px;
